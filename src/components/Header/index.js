8d77f5d204afe22ca483085e2d30dbc5d669a116
Diego <diego.fernandesjs@gmail.com>
2024-10-01 20:27:03 -0300

    Update

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+    height: 142px;
+    background-image: url('backgroundHeader.png');
+`
+function HeaderComponents() {
+    return (
+        <Header> Teste</Header>
+    )
+}
+
+export default HeaderComponents;
