a4eb437c3124474efd4f5dbc81fc3c05f3868795
Diego <diego.fernandesjs@gmail.com>
2024-10-03 20:08:26 -0300

    Create component logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const Logo = styled.div`
+    font-size: 70px;
+    font-weight: 700;
+    letter-spacing: -5px;
+    color: black;
+    
+`
+
+function Logotipo() {
+    return(
+        <Logo>Sable</Logo>
+    )
+}
+
+export default Logotipo;
