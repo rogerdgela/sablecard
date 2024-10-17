a0a1cf0b23eb9d4e14b6be2e71b914a848791a61
Rogerio Silva <dgelask8@gmail.com>
2024-10-17 20:53:04 -0300

    Parada pra descanso

---
+++ src/components/SectionCashback/index.js
@@ -0,0 +1,18 @@
+import { Container, Content, LeftHandSide, RightHandSide } from "./styles";
+import ContentBoxSide from "../../components/ContentBoxSide"
+
+export default function Component() {
+    return (
+        <Container>
+            <Content>
+                <LeftHandSide>
+                    <ContentBoxSide title="Earn 2% cash back on favorite brands" description="Earn 2%1 cash back on favorite brands, 1%1 cashback on all purchases, and a 2x cashback match in your first year! Get premium benefits and much more." cor="rgba(255, 255, 255, 0.98)"/>
+                    <ContentBoxSide title="Moving to the U.S.? No SSN required" description="Start building credit before you even enter the country. Sign up with your international address, passport & visa instead**." />
+                    <ContentBoxSide title="Get help from a real person" description="When you’ve got questions about your finances, get multilingual customer support anytime you need from direct in-app chat, email, phone, and our site FAQs." />
+                </LeftHandSide>
+                <RightHandSide>
+                </RightHandSide>
+            </Content>
+        </Container>
+    )
+}
