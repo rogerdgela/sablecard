19a3c1cd64cb9ba6763bb709e96064c73f8bedbb
Rogerio Silva <dgelask8@gmail.com>
2024-10-31 14:01:10 -0300

    Alterei para dispositivos ate 768px

--- src/components/CallMkt/styles.js
+++ src/components/CallMkt/styles.js
@@ -83,16 +83,32 @@ export const SectionTwo = styled.div`
     margin-top: 80px;
     margin-bottom: 220px;
 
-    @media (max-width: 480px) {
+    @media (max-width: 768px) {
         display: flex;
         flex-direction: row;
         justify-content: center;
-        align-items: center;
         width: 100%;
         flex-wrap: wrap;
-        gap: 30px;
+        gap: 20px;
         margin-top: 50px;
         margin-bottom: 30px;
+        background-image: url('/images/background-call.png'); 
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: contain;
+        
+        @media (max-width: 480px) {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            flex-wrap: wrap;
+            gap: 30px;
+            margin-top: 50px;
+            margin-bottom: 30px;
+            background: none;
+        }
     }
 `;
 
@@ -101,7 +117,7 @@ export const SectionSmart = styled.img`
     height: 884px;
     top: 950px;
 
-    @media (max-width: 480px) {
+    @media (max-width: 768px) {
         display: none;
     }
 `
@@ -111,9 +127,15 @@ export const Box = styled.div.withConfig({
 })`
     text-align: ${({ isEven }) => (isEven ? 'right' : 'left')};
 
-    @media (max-width: 480px) {
+    @media (max-width: 768px) {
+        width: 48%;
         text-align: center;
-        padding: 10px;
+
+        @media (max-width: 480px) {
+            width: 95%;
+            text-align: center;
+            padding: 10px;
+        }
     }
 `;
 
@@ -132,10 +154,16 @@ export const BoxTitle = styled.h2.withConfig({
     padding-right: ${({ isRight }) => (isRight ? '' : '320px')};
     padding-left: ${({ isRight }) => (isRight ? '320px' : '')};
 
-    @media (max-width: 480px) {
+    @media (max-width: 768px) {
         margin-top: 10px;
         text-align: center;
         padding: 10px;
+
+        @media (max-width: 480px) {
+            margin-top: 10px;
+            text-align: center;
+            padding: 10px;
+        }
     }
 `
 
@@ -149,8 +177,11 @@ export const BoxDescription = styled.p.withConfig({
     padding-right: ${({ isRight }) => (isRight ? '' : '330px')};
     padding-left: ${({ isRight }) => (isRight ? '330px' : '')};
 
-    @media (max-width: 480px) {
+    @media (max-width: 768px) {
         padding: 0;
+        color: white;
+        font-size: 14px;
+        margin-top: 2px;
     }
 `
 
